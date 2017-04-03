bcacd297305f9373aa6c0b637450c77dfa0535a6
John Lombardo <john@programbo.com>
2017-04-03 21:46:53 +0800

    Remove unnecessary abstraction

--- components/settings/users.js
+++ components/settings/users.js
@@ -1,7 +1,36 @@
-import { Tab } from 'material-ui/Tabs';
-import FontIcon from 'material-ui/FontIcon';
+import { observer } from 'mobx-react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
+// import User from '~/components/user';
 import { users } from '~/stores';
-import UserList from '~/components/userList';
 
-export default () => <UserList users={users} />;
+export default observer(() => (
+  <Table>
+    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+      <TableRow>
+        <TableHeaderColumn>Avatar</TableHeaderColumn>
+        <TableHeaderColumn>Display Name</TableHeaderColumn>
+        <TableHeaderColumn>Full Name</TableHeaderColumn>
+        <TableHeaderColumn>Email</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody displayRowCheckbox={false}>
+      {users.values.length &&
+        users.values.map(({ email }, index) => (
+          <TableRow key={index}>
+            <TableRowColumn>Picture</TableRowColumn>
+            <TableRowColumn>John</TableRowColumn>
+            <TableRowColumn>John Lombardo</TableRowColumn>
+            <TableRowColumn>{email}</TableRowColumn>
+          </TableRow>
+        ))}
+    </TableBody>
+  </Table>
+));
--- components/user.js
+++
@@ -1,9 +0,0 @@
-import { observer } from 'mobx-react';
-import { TableRow, TableRowColumn } from 'material-ui/Table';
-
-export default observer(({ email }) => (
-  <TableRow>
-    <TableRowColumn>Picture</TableRowColumn>
-    <TableRowColumn>{email}</TableRowColumn>
-  </TableRow>
-));
