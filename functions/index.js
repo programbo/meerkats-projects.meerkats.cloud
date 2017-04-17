5b52d03043d52a1c5bbe13ae7d3eb36d6e6e6b06
John Lombardo <john@programbo.com>
2017-04-17 17:45:26 +0800

    Re-enable cleanupUserData

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,2 @@
-// exports.cleanupUserData = require('./lib/cleanupUserData')
+exports.cleanupUserData = require('./lib/cleanupUserData')
 exports.optimizeAvatar = require('./lib/optimizeAvatar')
-// exports.createThumbnail = require('./lib/createThumbnail')
